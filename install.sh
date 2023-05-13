d1264daf46c2c767e64241d4b591d713925a8d89
Manu <manuelperezpinar@gmail.com>
2023-05-13 11:16:30 +0200

    Add compileprogram to installation script

--- install.sh
+++ install.sh
@@ -4,6 +4,17 @@ cp startlab /usr/local/bin/startlab
 cp submitlab /usr/local/bin/submitlab
 cp runprogram /usr/local/bin/runprogram
 cp testprogram /usr/local/bin/testprogram
+cp compileprogram /usr/local/bin/compileprogram
 
-echo "alias rp=runprogram" >> $HOME/.bashrc
-echo "alias tp=testprogram" >> $HOME/.bashrc
+if [ -z $(cat $HOME/.bashrc | grep "alias rp") ]
+then
+    echo "alias rp=runprogram" >> $HOME/.bashrc
+fi
+if [ -z $(cat $HOME/.bashrc | grep "alias tp") ]
+then
+    echo "alias tp=testprogram" >> $HOME/.bashrc
+fi
+if [ -z $(cat $HOME/.bashrc | grep "alias compile") ]
+then
+    echo "alias compile=compileprogram" >> $HOME/.bashrc
+fi
