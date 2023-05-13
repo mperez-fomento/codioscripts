7b7252dbcdf2e8cf7a268cb9e7d050f37197aa64
Manu <manuelperezpinar@gmail.com>
2023-05-13 14:22:15 +0200

    Create alias for startlab changing current folder

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ cp runprogram /usr/local/bin/runprogram
 cp testprogram /usr/local/bin/testprogram
 cp compileprogram /usr/local/bin/compileprogram
 
+if ! grep -q 'alias startlab' $HOME/.bashrc
+then
+    echo "alias startlab='. startlab'" >> $HOME/.bashrc
+fi
 if ! grep -q 'alias rp' $HOME/.bashrc
 then
     echo "alias rp=runprogram" >> $HOME/.bashrc
